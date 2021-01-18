ea221d85b3a32506b3e9a70825cc56a96a1fad4a
sthrace <71982194+sthrace@users.noreply.github.com>
2021-01-18 23:18:41 +0300

    Add files via upload
    Sort of working GNL

--- get_next_line.c
+++ get_next_line.c
@@ -1,54 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sthrace <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/01 12:44:47 by sthrace           #+#    #+#             */
-/*   Updated: 2020/12/01 12:44:49 by sthrace          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-void    *treat_leftover(char **lft, char **line) // if leftover contains more than 1 backslash n then only the final line will be returned
+void    *treat_leftover(char *lft, char **line) // if leftover contains more than 1 backslash n then only the final line will be returned
 {
     char        *ptr;
 
-    if ((ptr = ft_strchr(*lft, '\n')))
+    ptr = NULL;
+    if (lft)
     {
-        *ptr = '\0';
-        *line = ft_strdup(*lft);
-        *lft = ft_strdup(++ptr);
+        if ((ptr = ft_strchr(lft, '\n')))
+        {
+            *ptr = '\0';
+            *line = ft_strdup(lft);
+            ft_strcpy(lft, ++ptr);
+        }
+        else
+        {
+            *line = ft_strdup(lft);
+            *lft = '\0';
+        }
     }
     else
     {
-        *line = ft_strdup(*lft);
-        *lft = NULL;
+        *line = (char *)malloc(sizeof(char) + 1);
+        **line = '\0';
     }
-    return (*line);
+    return (ptr);
 }
 
 int     get_next_line(int fd, char **line)
 {
     int         res;
-    char        buff[B_SIZE + 1];
+    char        buff[BUFFER_SIZE + 1];
     char        *ptr;
     static char *lft;
 
-    while (lft)
-        treat_leftover(&lft, line);
-    while ((res = read(fd, buff, B_SIZE)))
+    ptr = treat_leftover(lft, line);   
+    while (!ptr && (res = read(fd, buff, BUFFER_SIZE)))
     {
-        buff[res] = '\0';
+        
         if ((ptr = (ft_strchr(buff, '\n'))))
         {
             *ptr = '\0';
-            *line = ft_strjoin(*line, buff);
             lft = ft_strdup(++ptr);
-            break;
         }
+        buff[res] = '\0';
         *line = ft_strjoin(*line, buff);
     }
     return (res);
@@ -62,8 +57,7 @@ int     main()
 
     fd = open("test.txt", O_RDONLY);
     while ((i = get_next_line(fd, &line)))
-        printf("Line: %s\n", line);
-    get_next_line(fd, &line);
-        printf("LineF: %s\n", line);
+        printf("Line: %s\n\n", line);
+    printf("LineF: %s\n\n", line);
     return (0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,29 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sthrace <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/01 12:45:03 by sthrace           #+#    #+#             */
-/*   Updated: 2020/12/01 12:45:05 by sthrace          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
-
-char	*ft_strnew(size_t size)
-{
-	char *str;
-
-	str = (char *)malloc(sizeof(char) * (size + 1));
-	if (str == NULL)
-		return (NULL);
-	while (size > 0)
-		str[size--] = '\0';
-	str[0] = '\0';
-	return (str);
-}
 
 size_t		ft_strlen(const char *s)
 {
@@ -33,6 +8,31 @@ size_t		ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
+}
+
+void		*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if ((dst == NULL) && (src == NULL))
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+char		*ft_strdup(const char *s1)
+{
+	char	*dst;
+
+	if (!(dst = (char *)malloc(ft_strlen(s1) + 1)))
+		return (NULL);
+	ft_memcpy(dst, s1, ft_strlen(s1) + 1);
+	return (dst);
 }
 
 char		*ft_strjoin(char const *s1, char const *s2)
@@ -67,19 +67,20 @@ char		*ft_strchr(const char *s, int c)
 	return (0);
 }
 
-char		*ft_strdup(const char *s1)
+char		*ft_strcpy(char *dst, const char *src)
 {
-	char	*dst;
+	size_t	srclen;
 	size_t	i;
 
-	if (!(dst = (char *)malloc(ft_strlen(s1) + 1)))
-		return (NULL);
+	if (dst == NULL && src == NULL)
+		return (0);
+	srclen = ft_strlen(src);
 	i = 0;
-	while (i < ft_strlen(s1) + 1)
+	while (src[i] != '\0')
 	{
-		((unsigned char *)dst)[i] = ((unsigned char *)s1)[i];
+		dst[i] = src[i];
 		i++;
 	}
+	dst[i] = '\0';
 	return (dst);
 }
-
